0ae5f7692c8351751c0fb0a10383c8262bea36f5
saqibhussain789 <shkb2003@gmail.com>
2025-02-06 00:03:51 +0500

    Added Next.js project files

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "./provider";
 
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import {withSentryConfig} from "@sentry/nextjs";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  typescript : {
+    ignoreBuildErrors: true,
+  }
 };
 
 export default withSentryConfig(nextConfig, {
